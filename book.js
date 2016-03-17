e19f976fd52fb14a249655459c76a3d3a838947d
Yurii Rashkovskii <yrashk@gmail.com>
2016-03-17 06:46:52 -0700

    Add 'versions' plugin to docs gitbook

--- book.js
+++ book.js
@@ -2,6 +2,11 @@ module.exports = {
     // Documentation for GitBook is stored under "docs"
     root: './docs',
     title: 'Eventchain Documentation',
-
+    plugins: [ "versions" ],
+    pluginsConfig: {
+        versions: {
+            type: "branches"
+        }
+    },
     gitbook: '3.0.0-pre.5'
 };
