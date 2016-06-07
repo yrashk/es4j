4bb6658310c7794368958d58377e03e9faed6a28
Yurii Rashkovskii <yrashk@gmail.com>
2016-06-07 17:28:20 +0700

    Problem: missing license headers
    Solution: add license headers

--- src/test/java/com/eventsourcing/cep/protocols/NameProtocolTest.java
+++ src/test/java/com/eventsourcing/cep/protocols/NameProtocolTest.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2016, All Contributors (see CONTRIBUTORS file)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.eventsourcing.cep.protocols;
 
 import com.eventsourcing.Command;
