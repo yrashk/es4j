5032a0597e9588c12caeeb69dea425d8d68036b6
Yurii Rashkovskii <yrashk@gmail.com>
2016-10-13 15:57:32 +0700

    Problem: boguspackage is leaked into the main source set
    This package was only intended for testing purposes.
    
    Solution: make sure it is only in the test source set

---
+++ eventsourcing-repository/src/test/java/boguspackage/BogusCommand.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016, All Contributors (see CONTRIBUTORS file)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package boguspackage;
+
+import com.eventsourcing.EventStream;
+import com.eventsourcing.StandardCommand;
+import com.eventsourcing.hlc.HybridTimestamp;
+import lombok.Builder;
+
+public class BogusCommand extends StandardCommand<Void, String> {
+
+    @Builder
+    public BogusCommand(HybridTimestamp timestamp) {
+        super(timestamp);
+    }
+
+    @Override
+    public EventStream<Void> events() throws Exception {
+        return EventStream.of(BogusEvent.builder().build());
+    }
+
+    @Override
+    public String result() {
+        return "bogus";
+    }
+}
---
+++ eventsourcing-repository/src/test/java/boguspackage/BogusEvent.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2016, All Contributors (see CONTRIBUTORS file)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package boguspackage;
+
+import com.eventsourcing.StandardEvent;
+import com.eventsourcing.hlc.HybridTimestamp;
+import com.eventsourcing.index.Index;
+import com.eventsourcing.index.SimpleIndex;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+import static com.eventsourcing.index.IndexEngine.IndexFeature.EQ;
+import static com.eventsourcing.index.IndexEngine.IndexFeature.SC;
+
+@Accessors(fluent = true)
+public class BogusEvent extends StandardEvent {
+    @Getter
+    private final String string;
+
+    @Index({EQ, SC})
+    public static SimpleIndex<BogusEvent, String> ATTR = BogusEvent::string;
+
+    @Builder
+    public BogusEvent(HybridTimestamp timestamp, String string) {
+        super(timestamp);
+        this.string = string == null ? "bogus" : string;
+    }
+}
