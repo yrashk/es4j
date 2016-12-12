1e8b8b45696e36aa594a58182d5252c9c4d5fd2c
Yurii Rashkovskii <yrashk@gmail.com>
2016-12-12 13:38:32 -0800

    Problem: Kotlin class analyzer use is ugly
    It interferes with the goal of simplicity of the library
    use.
    
    Solution: Switch to Kotlin 1.1-M3 (1.1 EAP) where Java 8
    parameter name generator has been implemented

--- eventsourcing-kotlin/src/test/kotlin/EntityTest.kt
+++ eventsourcing-kotlin/src/test/kotlin/EntityTest.kt
@@ -10,15 +10,12 @@ import com.eventsourcing.StandardEvent
 import com.eventsourcing.index.JavaStaticFieldIndexLoader
 import com.eventsourcing.index.MemoryIndexEngine
 import com.eventsourcing.index.SimpleIndex
-import com.eventsourcing.kotlin.KotlinClassAnalyzer
-import com.eventsourcing.layout.UseClassAnalyzer
 import org.testng.annotations.Test
 import kotlin.test.assertTrue
 
-@UseClassAnalyzer(KotlinClassAnalyzer::class)
 class TestEntity(val x: String) : StandardEvent() {
     companion object {
-        @JvmField var X = SimpleIndex { o: TestEntity -> o.x }
+        @JvmField var X = SimpleIndex.`as` { o: TestEntity -> o.x }
     }
 }
 
