829b78ce3a0b4ce5a371e29bf58cd1a556d8db8f
Yurii Rashkovskii <yrashk@gmail.com>
2017-01-29 10:47:46 +0700

    Problem: Min & Max queries share a lot of code with little difference
    This seriously impacts overall readability and makes further improvements
    or extensions much more difficult.
    
    Solution: abstract that code away into ComparingQuery

--- eventsourcing-core/src/main/java/com/eventsourcing/queries/Max.java
+++ eventsourcing-core/src/main/java/com/eventsourcing/queries/Max.java
@@ -9,89 +9,21 @@ package com.eventsourcing.queries;
 
 
 import com.eventsourcing.Entity;
-import com.eventsourcing.EntityHandle;
 import com.eventsourcing.index.EntityIndex;
-import com.googlecode.cqengine.IndexedCollection;
-import com.googlecode.cqengine.attribute.Attribute;
-import com.googlecode.cqengine.attribute.SimpleAttribute;
-import com.googlecode.cqengine.query.option.QueryOptions;
-import com.googlecode.cqengine.query.simple.Equal;
-import com.googlecode.cqengine.query.simple.SimpleQuery;
 
-import java.util.Iterator;
-
-public class Max<O extends Entity, A extends Comparable<A>> extends SimpleQuery<EntityHandle<O>, A>  {
-
-    private EntityHandle<O> max = null;
-    private void ensureMaxIsFound(SimpleAttribute<EntityHandle<O>, A> attribute, QueryOptions queryOptions) {
-        if (max == null) {
-            IndexedCollection<EntityHandle<O>> collection = queryOptions.get(IndexedCollection.class);
-            if (collection == null) {
-                throw new RuntimeException(
-                        toString() + " has to be supported by the target index or queryOptions should" +
-                                " include IndexedCollection key");
-            }
-            Iterator<EntityHandle<O>> iterator = collection.iterator();
-            A maxValue = null;
-            while (iterator.hasNext()) {
-                EntityHandle<O> next = iterator.next();
-                A value = attribute.getValue(next, queryOptions);
-                if (max == null || value.compareTo(maxValue) > 0) {
-                    max = next;
-                    maxValue = value;
-                }
-            }
-        }
-    }
-    private void ensureMaxIsFound(Attribute<EntityHandle<O>, A> attribute, QueryOptions queryOptions) {
-        if (max == null) {
-            IndexedCollection<EntityHandle<O>> collection = queryOptions.get(IndexedCollection.class);
-            if (collection == null) {
-                throw new RuntimeException(
-                        toString() + " has to be supported by the target index or queryOptions should" +
-                                " include IndexedCollection key");
-            }
-            Iterator<EntityHandle<O>> iterator = collection.iterator();
-            A maxValue = null;
-            while (iterator.hasNext()) {
-                EntityHandle<O> next = iterator.next();
-                Iterable<A> values = attribute.getValues(next, queryOptions);
-                for (A value : values) {
-                    if (max == null || value.compareTo(maxValue) > 0) {
-                        max = next;
-                        maxValue = value;
-                    }
-                }            }
-        }
-    }
+public class Max<O extends Entity, A extends Comparable<A>> extends ComparingQuery<O, A>  {
 
     public Max(EntityIndex<O, A> index) {
-        super(index.getAttribute());
+        super(index);
     }
 
-    @Override
-    protected boolean matchesSimpleAttribute(SimpleAttribute<EntityHandle<O>, A> attribute, EntityHandle<O> object,
-                                             QueryOptions queryOptions) {
-        ensureMaxIsFound(attribute, queryOptions);
-        return max.uuid().equals(object.uuid());
-    }
-
-    @Override
-    protected boolean matchesNonSimpleAttribute(
-            Attribute<EntityHandle<O>, A> attribute, EntityHandle<O> object,
-            QueryOptions queryOptions) {
-        ensureMaxIsFound(attribute, queryOptions);
-        return max.uuid().equals(object.uuid());
-    }
-
-    @Override protected int calcHashCode() {
-        int result = attribute.hashCode();
-        return result;
+    @Override protected boolean isBetterValue(A value, A targetValue) {
+        return value.compareTo(targetValue) > 0;
     }
 
     @Override
     public String toString() {
-        return "max(" + asLiteral(super.getAttributeName()) + ")";
+        return "max(" + asLiteral(attribute.getAttributeName()) + ")";
     }
 
     @Override public boolean equals(Object o) {
@@ -104,4 +36,5 @@ public class Max<O extends Entity, A extends Comparable<A>> extends SimpleQuery<
 
         return true;
     }
+
 }
--- eventsourcing-core/src/main/java/com/eventsourcing/queries/Min.java
+++ eventsourcing-core/src/main/java/com/eventsourcing/queries/Min.java
@@ -19,78 +19,19 @@ import com.googlecode.cqengine.query.simple.SimpleQuery;
 
 import java.util.Iterator;
 
-public class Min<O extends Entity, A extends Comparable<A>> extends SimpleQuery<EntityHandle<O>, A>  {
-
-    private EntityHandle<O> min = null;
-    private void ensureMinIsFound(SimpleAttribute<EntityHandle<O>, A> attribute, QueryOptions queryOptions) {
-        if (min == null) {
-            IndexedCollection<EntityHandle<O>> collection = queryOptions.get(IndexedCollection.class);
-            if (collection == null) {
-                throw new RuntimeException(
-                        toString() + " has to be supported by the target index or queryOptions should" +
-                                " include IndexedCollection key");
-            }
-            Iterator<EntityHandle<O>> iterator = collection.iterator();
-            A minValue = null;
-            while (iterator.hasNext()) {
-                EntityHandle<O> next = iterator.next();
-                A value = attribute.getValue(next, queryOptions);
-                if (min == null || value.compareTo(minValue) < 0) {
-                    min = next;
-                    minValue = value;
-                }
-            }
-        }
-    }
-    private void ensureMinIsFound(Attribute<EntityHandle<O>, A> attribute, QueryOptions queryOptions) {
-        if (min == null) {
-            IndexedCollection<EntityHandle<O>> collection = queryOptions.get(IndexedCollection.class);
-            if (collection == null) {
-                throw new RuntimeException(
-                        toString() + " has to be supported by the target index or queryOptions should" +
-                                " include IndexedCollection key");
-            }
-            Iterator<EntityHandle<O>> iterator = collection.iterator();
-            A minValue = null;
-            while (iterator.hasNext()) {
-                EntityHandle<O> next = iterator.next();
-                Iterable<A> values = attribute.getValues(next, queryOptions);
-                for (A value : values) {
-                    if (min == null || value.compareTo(minValue) < 0) {
-                        min = next;
-                        minValue = value;
-                    }
-                }            }
-        }
-    }
+public class Min<O extends Entity, A extends Comparable<A>> extends ComparingQuery<O, A>  {
 
     public Min(EntityIndex<O, A> index) {
-        super(index.getAttribute());
+        super(index);
     }
 
-    @Override
-    protected boolean matchesSimpleAttribute(SimpleAttribute<EntityHandle<O>, A> attribute, EntityHandle<O> object,
-                                             QueryOptions queryOptions) {
-        ensureMinIsFound(attribute, queryOptions);
-        return min.uuid().equals(object.uuid());
-    }
-
-    @Override
-    protected boolean matchesNonSimpleAttribute(
-            Attribute<EntityHandle<O>, A> attribute, EntityHandle<O> object,
-            QueryOptions queryOptions) {
-        ensureMinIsFound(attribute, queryOptions);
-        return min.uuid().equals(object.uuid());
-    }
-
-    @Override protected int calcHashCode() {
-        int result = attribute.hashCode();
-        return result;
+    @Override protected boolean isBetterValue(A value, A targetValue) {
+        return value.compareTo(targetValue) < 0;
     }
 
     @Override
     public String toString() {
-        return "min(" + asLiteral(super.getAttributeName()) + ")";
+        return "min(" + asLiteral(attribute.getAttributeName()) + ")";
     }
 
     @Override public boolean equals(Object o) {
